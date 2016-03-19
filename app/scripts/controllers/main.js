045572de2dd6ac587d4a656d5a2791d6e8416fe8
leomartindev <leo1.martin@epitech.eu>
2016-03-19 16:21:57 +0100

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name remonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the remonApp
+ */
+angular.module('remonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
